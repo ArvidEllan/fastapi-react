e9c54227d90a029ae8bebc8b608cf59720d5ae4b
“ArvidEllan” <ellanwambugu6@gmail.com>
2023-06-15 22:44:09 +0300

    fastapi-react push A

---
+++ app/api.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to your todo list."}
+#########################################CRUD OPERATIONS ##
+#GET
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    }
+]
+#router
+@app.get("/Todo", tags=["todos"])
+async def get_todos() -> dict:
+    return { "data": todos }
