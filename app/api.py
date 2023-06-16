bf4bae3e826337b7067ee86aa90dd433b8b43403
Ellan G <99280261+ArvidEllan@users.noreply.github.com>
2023-06-16 09:46:30 +0300

    PUT router

--- app/api.py
+++ app/api.py
@@ -45,3 +45,16 @@ async def add_todo(todo: dict) -> dict:
     return {
         "data": { "Todo added." }
     }
+###PUT##
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todo["item"] = body["item"]
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
