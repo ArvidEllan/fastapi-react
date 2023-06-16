e62e2ee90e0919a06968cb61e86fe066ec5fa7f3
Ellan G <99280261+ArvidEllan@users.noreply.github.com>
2023-06-16 09:44:32 +0300

    update on function AddTodo

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,43 +1,87 @@
-import React, { useEffect, useState } from "react";
+
+import React, { useEffect, useState, useContext } from "react";
 import {
-    Box,
-    Button,
-    Flex,
-    Input,
-    InputGroup,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-    Stack,
-    Text,
-    useDisclosure
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
 
 const TodosContext = React.createContext({
-  todos: [], fetchTodos: () => {}
-})
+  todos: [],
+  fetchTodos: () => {},
+});
 
 export default function Todos() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
   const fetchTodos = async () => {
-    const response = await fetch("http://localhost:8000/todo")
-    const todos = await response.json()
-    setTodos(todos.data)
-  }
+    try {
+      const response = await fetch("http://localhost:8000/todo");
+      const todosData = await response.json();
+      setTodos(todosData.data);
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
   useEffect(() => {
-    fetchTodos()
-  }, [])
+    fetchTodos();
+  }, []);
+
   return (
-    <TodosContext.Provider value={{todos, fetchTodos}}>
+    <TodosContext.Provider value={{ todos, fetchTodos }}>
+      <AddTodo />
       <Stack spacing={5}>
         {todos.map((todo) => (
-          <b>{todo.item}</b>
+          <Text key={todo.id}>{todo.item}</Text>
         ))}
       </Stack>
     </TodosContext.Provider>
-  )
+  );
+}
+
+function AddTodo() {
+  const [item, setItem] = useState("");
+  const { todos, fetchTodos } = useContext(TodosContext);
+
+  const handleInput = (event) => {
+    setItem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newTodo = {
+      id: todos.length + 1,
+      item: item,
+    };
+
+    fetch("http://localhost:8000/todo", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo),
+    })
+      .then(() => {
+        fetchTodos();
+      })
+      .catch((error) => {
+        console.error("Error adding todo:", error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputGroup size="md">
+        <Input
+          pr="4.5rem"
+          type="text"
+          placeholder="Add a todo item"
+          aria-label="Add a todo item"
+          onChange={handleInput}
+        />
+      </InputGroup>
+    </form>
+  );
 }
